6e8201b5b4d7699d0c9a02c51850fa431c5e2801
navyas211@gmail.com <navyas211@gmail.com>
2021-09-02 15:00:58 +0530

    [NAVYA]ADDED: Calculating Emp Wages for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,22 +2,28 @@
 
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=20;
-empCheck=$((RANDOM%2));
+totalSalary=0;
+empRatePerHrs=20;
+workingHrs=15;
+numWorkingDay=20;
+day=0;
 
-case $empCheck in
-          $isFullTime)
-               empHrs=8
-               ;;
-
-          $isPartTime)
-                empHrs=4
-               ;;
-         *)
-          empHrs=0
-                     ;;
+while [ $day -le $numWorkingDay -a $day -le $workingHrs ]
+do
+  empCheck=$((RANDOM%3));
+          case $empCheck in
+                $isFullTime)
+                      empHrs=8
+                       ;;
+               $isPartTime)
+                      empHrs=4
+                       ;;
+                    *)
+                    empHrs=0
+                       ;;
 esac
-
-salary=$(($empHrs*$empRatePerHr));
-echo $salary
+(( day++ ))
+salary=$(($empHrs*$empRatePerHrs));
+totalSalary=$(($totalSalary+$salary));
+done
 
