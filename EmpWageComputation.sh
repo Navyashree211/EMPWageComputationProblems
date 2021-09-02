7410f5a9def677a61826daa8def6aec0fa158cc6
navyas211@gmail.com <navyas211@gmail.com>
2021-09-02 15:33:09 +0530

    [NAVYA]ADDED: Till Condition Working Hours Days

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,26 +4,27 @@ isPartTime=1;
 isFullTime=2;
 totalSalary=0;
 empRatePerHrs=20;
-workingHrs=15;
+numWorkingHrs=0;
 numWorkingDay=20;
-day=0;
+limitHrs=100;
+workingdays=1;
 
-while [ $day -le $numWorkingDay -a $day -le $workingHrs ]
+while [[ $numWorkingHrs -le $limitHrs && $workingdays -le $numWorkingDay ]]
 do
   empCheck=$((RANDOM%3));
-          case $empCheck in
-                $isFullTime)
-                      empHrs=8
-                       ;;
-               $isPartTime)
-                      empHrs=4
-                       ;;
-                    *)
-                    empHrs=0
-                       ;;
+           case $empCheck in
+                  $isFullTime)
+                          empHrs=8
+                            ;;
+                  $isPartTime)
+                          empHrs=4
+                            ;;
+                   *)
+                  empHrs=0
+                      ;;
 esac
-(( day++ ))
-salary=$(($empHrs*$empRatePerHrs));
-totalSalary=$(($totalSalary+$salary));
+numWorkigHrs=$(($numWorkingHrs + $empHrs));
+(( workingday++ ))
+salary=$(($empRatePerHrs * $empHrs));
+totalSalary=$(($totalSalary + $salary));
 done
-
