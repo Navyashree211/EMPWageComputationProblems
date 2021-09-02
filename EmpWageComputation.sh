c525bf6a6333872740f913e5a06afc07722bccad
navyas211@gmail.com <navyas211@gmail.com>
2021-09-02 14:46:23 +0530

    [NAVYA]ADDED: Add Part Time Emp

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,16 +1,19 @@
+
 #!/bin/bash -x
 
-echo "Welcome Employee Wage Problem"
-
-Full_Time=1
-randomCheck=$((RANDOM%3))
+IS_PART_TIME=1
+IS_FULL_TIME=2
 Emp_Rate_Perhr=20
-if [ $Full_Time -eq $randomCheck ]
+
+randomCheck=$((RANDOM%3))
+if [ $IS_PART_TIME -eq $randomCheck ]
+then
+    empHr=4
+elif [ $IS_FULL_TIME -eq $randomCheck ]
 then
     empHr=8
 else
     salary=0
 fi
-salary=$(($Emp_Rate_Perhr * $empHr))
+salary=$(($Emp_Rate_Perhr * $empHr));
 echo $salary
-
