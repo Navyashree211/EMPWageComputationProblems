2629ed12a45d91aff8401bcc4605c0cc94a96beb
navyas211@gmail.com <navyas211@gmail.com>
2021-09-04 22:36:49 +0530

    [NAVYA]ADDED: Store Day and Daily wage along with total wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,16 +2,17 @@
 
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=50;
-EMP_RATE_PER_HR=8;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
+declare -A dailyWage
 function getWorkHrs() {
-             $empCheck = $1
+            $empCheck = $1
 
-        case $empCheck in
+     case $empCheck in
              $IS_FULL_TIME)
                        empHrs=8
                         ;;
@@ -24,20 +25,18 @@ function getWorkHrs() {
 esac
 echo $empHrs
 }
-
 function getEmpWage() {
-   local empHr=$1
+   local empHrs=$1
    echo  $(($empHrs*$EMP_RATE_PER_HR))
 }
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-   ((totalWorkingDays++))
-    empCheck=$((RANDOM%3));
-    empHrs="$( getWorkHrs $empCheck )"
-    totalEmpHrs=$(($totalEmpHrs+$empHrs))
-    dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
+  ((totalWorkingDays++))
+  empCheck=$((RANDOM%3));
+   empHrs="$( getWorkHrs $empCheck )"
+   totalEmpHrs=$(($totalEmpHrs+$empHrs))
+   dailyWage["Day $totalWorkingDays"]="$( getEmpWage $empHrs )"
 done
-
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
 echo ${dailyWage[@]}
-
+echo ${!dailyWage[@]}
